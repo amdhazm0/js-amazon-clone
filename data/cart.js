ef570ecd6449c47eb1a95c5300427703dbc229c2
amdhazm0 <amdhazm0@gmail.com>
2025-05-08 20:45:25 +0300

    Initialize cart with default items and implement delivery options in checkout

--- data/cart.js
+++ data/cart.js
@@ -1,6 +1,14 @@
 export const cart = localStorage.getItem('cart')
   ? JSON.parse(localStorage.getItem('cart'))
-  : []; 
+  : [{
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2,
+    deliveryOptionId: "1",
+  }, {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d", 
+    quantity: 1,
+    deliveryOptionId: "2",
+  }];
 
 export function saveCartToLocalStorage() {
     localStorage.setItem('cart', JSON.stringify(cart));
@@ -31,6 +39,7 @@ export function saveCartToLocalStorage() {
       cart.push({
         productId: productId,
         quantity: Number(quantitySelector.value),
+        deliveryOptionId: "1",
       });
     }
     quantitySelector.value = 1;
